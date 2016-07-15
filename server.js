f66e510e745a6554cc0accb9d4528ff62566a8cd
Basil <Basil.beltran@gmail.com>
2016-07-15 11:10:01 -0600

    merged basic angular address book

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
+require('cute-stack')('pretty');
+var path = require('path');
+var debug = require('debug');
+var log = debug('server:log');
+var info = debug('server:info');
+var error = debug('server:error');
+
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname + '/public'));  // configure a public directory to host static content
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+var bp = require('body-parser');
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
 
-// configure a public directory to host static content
-app.use(express.static(__dirname + '/public'));
-
-require ("./test/app.js")(app);
+// require ("./test/app.js")(app);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3001;
-console.log('will listen on '+port)
-app.listen(port, ipaddress);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3001);   //set and OR
+
+app.listen(app.get('port'), () => {              //only start if we have a db
+  info('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>listening on '+app.get('port') )
+});
