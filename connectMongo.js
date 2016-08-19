e5a7fa6cf73a965121644f838b385c592dde910c
Basil <Basil.beltran@gmail.com>
2016-08-18 23:30:01 -0600

    connected node and mongo

--- connectMongo.js
+++ connectMongo.js
@@ -2,8 +2,11 @@ var mongodb = require('mongodb');
 var muri_mlab1 = 'mongodb://fuser:password1@ds017165.mlab.com:17165/foenix_db1'
 var muri_mlab2 = 'mongodb://fuser2:mongodb777@ds017165.mlab.com:17165/foenix_db1'
 var muri_local_movies = 'mongodb://localhost:27017/movies';
+var muri_local_example = 'mongodb://localhost:27017/example';
 
-var uri = muri_local_movies;
+var uri = muri_local_example;
+
+
 module.exports = function(callback) {
   mongodb.MongoClient.connect(uri, callback);
 };
--- index.js
+++ index.js
@@ -34,10 +34,26 @@ var server = app.listen(port, () => {
     console.log('Server up!', port);
 });
 
-var connect = require('./connectMongo');
-//require ("./mongoTest/app.js")(app);
-//var movies = require('./movies');
-//var dbInterface = require('./interface');
+//  var connect = require('./connectMongo'); // edx m101
+//  require ("./testMongo/app.js")(app);  //cs5610
+
+var db
+const MongoClient = require('mongodb').MongoClient; // mongodb://fuser2:<mongodb777>@ds017165.mlab.com:17165/foenix_db1
+MongoClient.connect('mongodb://localhost:27017/example', (err, database) => {
+  if (err) {
+    console.log(error);
+  }
+  db = database
+
+  database.collection('sample').insert({ myKey: "YESSS!" }, function(error, result) {
+    if (error) {
+      console.log(error);
+    }
+  });
+
+});
+
+
 
 
 var socketServer = io(server);
@@ -51,9 +67,5 @@ socketServer.on('connection', (socket) => {  // socketServer for broadcast,  soc
 // The browser has sent a statsObj
     socket.on('stats', (stats) => {
       console.log(JSON.stringify(stats, null, 2) );
-
     });
-
 });
-
-//return app  }
